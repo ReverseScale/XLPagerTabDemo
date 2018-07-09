0cb859dcd765f2df935be07552e9195f40647e47
ReverseScale <ReverseScale@iCloud.com>
2018-07-09 20:25:06 +0800

    ✨:Introducing simple example features.

---
+++ XLPagerTabDemo/InstagramExampleViewController.swift
@@ -0,0 +1,45 @@
+//
+//  InstagramExampleViewController.swift
+//  XLPagerTabDemo
+//
+//  Created by Steven Xie on 2018/7/9.
+//  Copyright © 2018年 Steven Xie. All rights reserved.
+//
+
+import Foundation
+import XLPagerTabStrip
+
+class InstagramExampleViewController: ButtonBarPagerTabStripViewController {
+
+    @IBOutlet weak var shadowView: UIView!
+    let blueInstagramColor = UIColor(red: 37/255.0, green: 111/255.0, blue: 206/255.0, alpha: 1.0)
+
+    override func viewDidLoad() {
+        // change selected bar color
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = blueInstagramColor
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = self?.blueInstagramColor
+        }
+        super.viewDidLoad()
+    }
+
+    // MARK: - PagerTabStripDataSource
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let child_1 = TableChildExampleViewController(style: .plain, itemInfo: "FOLLOWING")
+        let child_2 = ChildExampleViewController(itemInfo: "YOU")
+        return [child_1, child_2]
+    }
+}
---
+++ XLPagerTabDemo/PostCell.swift
@@ -0,0 +1,37 @@
+//
+//  PostCell.swift
+//  XLPagerTabDemo
+//
+//  Created by Steven Xie on 2018/7/9.
+//  Copyright © 2018年 Steven Xie. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var postName: UILabel!
+    @IBOutlet weak var postText: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        userImage.layer.cornerRadius = 10.0
+    }
+
+    func configureWithData(_ data: NSDictionary) {
+        if let post = data["post"] as? NSDictionary, let user = post["user"] as? NSDictionary {
+            postName.text = user["name"] as? String
+            postText.text = post["text"] as? String
+            userImage.image = UIImage(named: postName.text!.replacingOccurrences(of: " ", with: "_"))
+        }
+    }
+
+    func changeStylToBlack() {
+        userImage?.layer.cornerRadius = 30.0
+        postText.text = nil
+        postName.font = UIFont(name: "HelveticaNeue-Light", size:18) ?? .systemFont(ofSize: 18)
+        postName.textColor = .white
+        backgroundColor = UIColor(red: 15/255.0, green: 16/255.0, blue: 16/255.0, alpha: 1.0)
+    }
+}

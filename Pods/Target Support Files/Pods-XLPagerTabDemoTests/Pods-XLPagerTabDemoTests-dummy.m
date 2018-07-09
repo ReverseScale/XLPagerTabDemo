fc4b1f055f0ea88c9ed60ea143a566c65bdfc81c
ReverseScale <ReverseScale@iCloud.com>
2018-07-09 20:07:50 +0800

    🎉:Initial commit.

---
+++ Pods/Target Support Files/Pods-XLPagerTabDemoTests/Pods-XLPagerTabDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XLPagerTabDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_XLPagerTabDemoTests
+@end
---
+++ Pods/Target Support Files/Pods-XLPagerTabDemoUITests/Pods-XLPagerTabDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XLPagerTabDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_XLPagerTabDemoUITests
+@end
